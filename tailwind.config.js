363cd47db655842f0973550536fe5f982144d678
NasKal <56295690+NasKal@users.noreply.github.com>
2025-03-20 20:58:05 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
+    fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+        serif: ['Merriweather', 'serif'],
+      },
   },
   plugins: [],
 };
